cfbbe7214bae9b5f46d81f4780ce29599b2314e5
DC-Code123 <distinctcode123@gmail.com>
2025-07-19 14:06:05 +0200

    learning rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,28 +2,20 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 use colored::Colorize;
+
 fn main() {
     println!("Welcome to my guessing game!");
     println!("Please enter the maximum number for the guessing range:1-100");
     loop {
-        let secret_number = secret_number();
+        let secret_number = gen_rand();
 
         println!("A secret number has been generated between 1 and 100.");
-        println!("Do you want a hint? (yes=y/no=n)");
 
-        let mut hint_choice = String::new();
-        io::stdin().read_line(&mut hint_choice).expect("Failed to read line");
-        let hint_choice = hint_choice.trim().to_lowercase();
-
-        if hint_choice == "yes" || hint_choice == "y" {
-            println!("Hint: The secret number 5 digits positive from {}.", &secret_number - 5);
-        } else {
-            println!("Enjoy the game without hints!");
-        }
+        offer_hint(secret_number);
 
         println!("Please enter your guess: ");
 
-        let mut guess = String::new(); //(1)
+        let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Failed to read line");
 
         if guess.trim().is_empty() {
@@ -31,9 +23,7 @@ fn main() {
             continue;
         }
 
-        let guess: i32 = match guess.trim().parse() {
-            //(2)&(1) are instances of shadowing where the variable `guess` is redefined
-            // to hold the parsed integer value. and this allow us to use the same variable name with different types
+        let guess: f64 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
                 println!("Invalid input, please enter a number.");
@@ -41,20 +31,20 @@ fn main() {
             }
         };
 
-        if guess < 1 || guess > 100 {
+        if guess < 1.0 || guess > 100.0 {
             println!("Please enter a number between 1 and 100.");
             continue;
         }
 
         println!("You inputted: {}", guess);
 
-        if guess == secret_number {
+        if (guess - secret_number).abs() < f64::EPSILON {
             println!("Congratulations! You guessed the number!");
         } else {
             println!("Sorry, the secret number was: {}", secret_number);
         }
 
-        match guess.cmp(&secret_number) {
+        match guess.partial_cmp(&secret_number).unwrap() {
             Ordering::Less =>
                 println!(
                     "{} {}",
@@ -81,49 +71,77 @@ fn main() {
             break;
         }
     }
-
-    /*
-    Data types
-        Data types in Rust are categorized into two main types: scalar and compound.
-        Scalar types represent a single value, while compound types can group multiple values together.
-        Scalar types include integers, floating-point numbers, booleans, and characters.
-        Compound types include tuples and arrays, which can hold multiple values of different or the same types.
-    */
-
-    /*
-    //tuples
-    let tuple = (42, 3.14, 'x'); // A tuple is a an array that can hold multiple values of different types
-    //To access tuple elements, you can use pattern matching or indexing
-    let (a, b, c) = tuple; // Pattern matching to extract values from the tuple
-    println!("Tuple values: {}, {}, {}", a, b, c);
-    let first_element = tuple.0; // Accessing the first element using indexing
-    println!("First element of the tuple: {}", first_element);
-    println!("Second element of the tuple: {}", tuple.1); // Accessing the second element using indexing
-    println!("The element in the tuple has {:?}", tuple);
-
-    //arrays
-    let array = [1, 2, 3, 4, 5];
-
-    // An array is a fixed-size collection of elements of the same type
-    println!("Array values: {:?}", array); // Using {:?} to print the array
-    println!("First element of the array: {}", array[0]); // Accessing the first
-    println!("Second element of the array: {}", array[1]); // Accessing the second element
-    println!("Third element of the array: {}", array[2]); // Accessing the third
-    println!("Fourth element of the array: {}", array[3]); // Accessing the fourth element
-    println!("Fifth element of the array: {}", array[4]); // Accessing the fifth element
-
-    // Note: Arrays in Rust are immutable by default, meaning you cannot change their elements after creation.
-    let mut array1 = [0; 5]; // An array with 5 elements, all initialized to 0
-    array1[2] = 10;*/
 }
 
-/*
-    By the way we know that variable are immutable(unable to be change) but they can sorta be changed 
-    still through re-assignment and other way but consts are constant(can't be changed) at all
-*/
-
-fn secret_number() -> i32 {
+fn gen_rand() -> f64 {
     let mut rng = rand::rng();
-    let num = rng.random_range(1..=100);
-    num
+    return rng.random_range(1.0..=100.0);
+}
+
+fn offer_hint(secret_number: f64) {
+    println!("Do you want a hint? (yes=y/no=n)");
+
+    let mut hint_choice = String::new();
+    io::stdin().read_line(&mut hint_choice).expect("Failed to read line");
+    let hint_choice = hint_choice.trim().to_lowercase();
+
+    if hint_choice == "yes" || hint_choice == "y" {
+        println!("input a random number between 1 and 7:");
+        let mut hint_input = String::new();
+        io::stdin().read_line(&mut hint_input).expect("Failed to read line");
+        let hint_input: i32 = match hint_input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input, please enter a number.");
+                return;
+            }
+        };
+        if hint_input < 1 || hint_input > 7 {
+            println!("Please enter a number between 1 and 7.");
+            return;
+        }
+        match hint_input {
+            1 =>
+                println!(
+                    "HINT:((x-2)*5)+((x/2)-23)= {:.2}",
+                    (secret_number - 2.0) * 5.0 + (secret_number / 2.0 - 23.0)
+                ),
+            2 =>
+                println!(
+                    "HINT:x + (3×4) - (10÷2) + (7-1) = {:.2}",
+                    secret_number + 3.0 * 4.0 - 10.0 / 2.0 + (7.0 - 1.0)
+                ),
+            3 =>
+                println!(
+                    "HINT:(x + (5×3)) ÷ (7-5) + (10-2×4) = {:.2}",
+                    (secret_number + 5.0 * 3.0) / (7.0 - 5.0) + (10.0 - 2.0 * 4.0)
+                ),
+            4 =>
+                println!(
+                    "HINT:x + (100÷10÷2) - (3+2) + (4×5) - (21÷3) = {:.2}",
+                    secret_number + 100.0 / 10.0 / 2.0 - (3.0 + 2.0) + 4.0 * 5.0 - 21.0 / 3.0
+                ),
+            5 =>
+                println!(
+                    "HINT:((x*3) + (24/(2+2))) - (5*(6-4)) + (10-(15/3)) = {:.2}",
+                    secret_number * 3.0 +
+                        24.0 / (2.0 + 2.0) -
+                        5.0 * (6.0 - 4.0) +
+                        (10.0 - 15.0 / 3.0)
+                ),
+            6 =>
+                println!(
+                    "HINT:(x + (5*(4-1))) * (10/(7-2)) - (3+(8/4)) = {:.2}",
+                    (secret_number + 5.0 * (4.0 - 1.0)) * (10.0 / (7.0 - 2.0)) - (3.0 + 8.0 / 4.0)
+                ),
+            7 =>
+                println!(
+                    "HINT: x + ((15×2)÷5) - (9÷(3+0)) + (4×(5-2)) = {:.2}",
+                    secret_number + (15.0 * 2.0) / 5.0 - 9.0 / (3.0 + 0.0) + 4.0 * (5.0 - 2.0)
+                ),
+            _ => println!("Invalid hint choice."),
+        }
+    } else {
+        println!("Enjoy the game without hints!");
+    }
 }
